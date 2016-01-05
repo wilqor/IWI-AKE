b13e36d59051b79a8d52a5b505cb63d43de5e1e6
Usy <usy@gmx.us>
2016-01-05 19:43:01 +0100

    Added similarity options to gui

--- AKE-gui.py
+++ AKE-gui.py
@@ -105,24 +105,50 @@ class Application(Frame):
                 showerror("Open File", "Failed to read file\n'%s'" % file_name)
             return
 
-    def load_secondary_dir(self):
-        dir_name = askdirectory()
-        if dir_name:
-            try:
-                self.open_secondary_dir_label["text"] = dir_name
-                self.secondary_dir_path = dir_name
-            except:
-                showerror("Open Directory", "Failed to read file\n'%s'" % dir_name)
-            return
+    def find_similar_command(self):
+        self.clear_similarities()
+        self.after(1, self.find_similar)
+
 
     def find_similar(self):
-        # TODO - placeholder
-        self.similar_articles = self.primary_file_path + "\n" + self.secondary_dir_path
+        main_provider = FileContentProvider(self.primary_file_path)
+        main_extractor = KeyphraseExtractor(main_provider)
+
+        master_dir_path = os.path.dirname(self.primary_file_path)
+        excluded_path = os.path.basename(self.primary_file_path)
+        comparison_file_paths = DirectoryContentLister(master_dir_path, excluded_path).get_content_list()
+        file_providers = []
+        for path in comparison_file_paths:
+            file_providers.append(FileContentProvider(path))
+        comparison_extractor = MultipleProvidersKeyphraseExtractor(file_providers)
+
+        keyphrases = main_extractor.extract_keyphrases_by_textrank()
+        self.similarity_top_keyphrases = main_extractor.get_top_keyphrases(keyphrases, 0.2)
+
+        self.similarity_comparison_keyphrases_map = comparison_extractor.extract_keyphrases_map_by_textrank()
+
         self.set_similarities()
 
+    def find_similar_wiki_command(self):
+        self.clear_similarities()
+        self.after(1, self.find_similar_wiki)
+
     def find_similar_wiki(self):
-        # TODO - placeholder
-        self.similar_articles = self.similar_wiki_entry.get()
+        main_provider = WikipediaContentProvider(self.similar_wiki_entry.get())
+        main_extractor = KeyphraseExtractor(main_provider)
+
+        page = WikipediaPageFinder(self.similar_wiki_entry.get()).get_wikipedia_page()
+        links = page.links
+        link_page_providers = []
+        for link in links:
+            link_page_providers.append(WikipediaContentProvider(link))
+        comparison_extractor = MultipleProvidersKeyphraseExtractor(link_page_providers)
+
+        keyphrases = main_extractor.extract_keyphrases_by_textrank()
+        self.similarity_top_keyphrases = main_extractor.get_top_keyphrases(keyphrases, 0.2)
+
+        self.similarity_comparison_keyphrases_map = comparison_extractor.extract_keyphrases_map_by_textrank()
+
         self.set_similarities()
 
     def clear_similarities(self):
@@ -131,10 +157,15 @@ class Application(Frame):
 
     def set_similarities(self):
         self.clear_similarities()
-        # TODO - placeholder
-        content = "TODO:\n" + self.similar_articles
+        threshold = float(self.similarities_weight_entry.get())
+        similarity = DocumentKeyphrasesComparator(self.similarity_top_keyphrases, self.similarity_comparison_keyphrases_map, threshold).compare()
+        content = System.get_document_similarity_string(similarity)
+
         self.similarity_text.insert(END, content)
         self.similarity_text.configure(state="disabled")
+
+    def apply_similarities(self):
+        self.set_similarities()
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -211,7 +242,7 @@ class Application(Frame):
 
         similarity_page = Frame(notebook)
 
-        self.primary_file_selection_label = Label(similarity_page, text="Select primary file:")
+        self.primary_file_selection_label = Label(similarity_page, text="Select master file:")
         self.primary_file_selection_label.grid(row=1, column=0, sticky=W)
 
         self.open_primary_file_button = Button(similarity_page, text="Browse file", command=self.load_primary_file, width=20)
@@ -220,28 +251,28 @@ class Application(Frame):
         self.open_primary_file_label = Label(similarity_page)
         self.open_primary_file_label.grid(row=2, column=1, sticky=W)
 
-
-        self.secondary_dir_selection_label = Label(similarity_page, text="Select whole directory:")
-        self.secondary_dir_selection_label.grid(row=3, column=0, sticky=W)
-
-        self.open_secondary_dir_button = Button(similarity_page, text="Browse directory", command=self.load_secondary_dir, width=20)
-        self.open_secondary_dir_button.grid(row=4, column=0, sticky=W)
-
-        self.open_secondary_dir_label = Label(similarity_page)
-        self.open_secondary_dir_label.grid(row=4, column=1, sticky=E+W)
-
-        self.find_similar_button = Button(similarity_page, text="Find similar", command=self.find_similar, width=20)
-        self.find_similar_button.grid(row=4, column=2, sticky=E)
+        self.find_similar_button = Button(similarity_page, text="Find similar", command=self.find_similar_command, width=20)
+        self.find_similar_button.grid(row=2, column=2, sticky=E)
 
 
-        self.similar_wiki_selection_label = Label(similarity_page, text="Select wiki articles (comma separated titles):")
+        self.similar_wiki_selection_label = Label(similarity_page, text="Select wiki article:")
         self.similar_wiki_selection_label.grid(row=5, column=0, columnspan=2, sticky=W)
 
         self.similar_wiki_entry = Entry(similarity_page)
         self.similar_wiki_entry.grid(row=6, column=0, columnspan=2, sticky=E+W)
 
-        self.find_similar_wiki_button = Button(similarity_page, text="Find similar on Wikipedia", command=self.find_similar_wiki, width=20)
+        self.find_similar_wiki_button = Button(similarity_page, text="Find similar on Wikipedia", command=self.find_similar_wiki_command, width=20)
         self.find_similar_wiki_button.grid(row=6, column=2, sticky=E)
+
+        self.weight_label = Label(similarity_page, text="Similarity threshold")
+        self.weight_label.grid(row=7, column=0, sticky=W)
+
+        self.similarities_weight_entry = Entry(similarity_page)
+        self.similarities_weight_entry.grid(row=8, column=0, sticky=W)
+        self.similarities_weight_entry.insert(END, '0.45')
+
+        self.apply_similarities_button = Button(similarity_page, text="Apply", command=self.apply_similarities, width=20)
+        self.apply_similarities_button.grid(row=8, column=2, sticky=E)
 
         self.similarity_text = ScrolledText(similarity_page, state="disabled")
         self.similarity_text.grid(row=9, columnspan=3, sticky=E+W+S)
